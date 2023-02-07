328c50b127e06803f0d5946086cc061ca153cc74
Joaoof <joaodeus400@gmail.com>
2023-02-07 17:27:06 -0300

    componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { Header } from './components/Header'
 import './global.css'
 
 import style from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return(
     <div>
     <Header />
 
+
     <div className={style.wrapper}>
-      <aside>
-        sidebar
-      </aside>
+      <Sidebar />
       <main></main>
     </div>
       
