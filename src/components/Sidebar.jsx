931b8a21076030d8d8a3e2cbceab7dc2d62b1990
Joaoof <joaodeus400@gmail.com>
2023-02-07 18:27:37 -0300

    finalizando sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import {PencilLine} from 'phosphor-react'
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -8,12 +10,16 @@ export function Sidebar() {
             alt="" />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/Joaoof.png" alt="" />
+
+            
                 <strong>João de Deus</strong>
                 <span>Web Developer</span>
             </div>
 
           <footer>
             <a href="#">
+                <PencilLine size={20}/>
                 Editar seu perfil
             </a>
           </footer>
