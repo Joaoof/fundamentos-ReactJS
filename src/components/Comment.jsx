213aa5cf6caf5d5001ea29669373cf0f9b36dbb0
Joaoof <joaodeus400@gmail.com>
2023-02-15 16:31:17 -0300

    component: comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp } from "phosphor-react";
+import { Trash} from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/Joaoof.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>João de Deus </strong>
+              <time title="08 de fevereiro as 16:10" dateTime="2023-02-08">
+                Há cerca de 2 horas atrás!
+              </time>
+            </div>
+            <button title="Deletar comentário">
+                <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! </p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,43 +1,59 @@
-import styles from './Post.module.css'
+import { Comment } from './Comment';
+import styles from "./Post.module.css";
 
 export function Post() {
-    return(
-     <article className={styles.post}>
-        <header>
-            <div className={styles.author}>
-                <img className={styles.avatar} src="https://github.com/Joaoof.png" alt="#" />
-                <div className={styles.authorInfo}>
-                    <strong>João de Deus</strong>
-                    <span>Web Developer</span>
-                </div>
-            </div>
-            <time title='08 de fevereiro as 16:10' dateTime="2023-02-08">Publicado há 1h</time>
-        </header>
-
-        <div className={styles.content}>
-            
-    <p>Fala galeraa 👋</p>
-
-    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-    <p>👉{' '}<a href="">jane.design/doctorcare</a></p>
-
-    <p><a href="">#novoprojeto</a>{' '} 
-    <a href=''>#nlw</a>{' '}
-    <a href=''>#rocketseat</a></p>
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/Joaoof.png"
+            alt="#"
+          />
+          <div className={styles.authorInfo}>
+            <strong>João de Deus</strong>
+            <span>Web Developer</span>
+          </div>
         </div>
+        <time title="08 de fevereiro as 16:10" dateTime="2023-02-08">
+          Publicado há 1h
+        </time>
+      </header>
 
-        <form className={styles.commentForm}>
-            <strong>Deixe seu feedback</strong>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
 
-        <textarea 
-             placeholder='Deixe um comentário'
-        />
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
 
         <footer>
-        <button type='submit'>Publicar</button>
+          <button type="submit">Publicar</button>
         </footer>
-        </form>
-     </article>
-    )
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
 }
