de040c6a36c724b28312a675353fc02979c7f1e4
Joaoof <joaodeus400@gmail.com>
2023-02-16 17:16:11 -0300

    component avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,31 @@
-import {PencilLine} from 'phosphor-react'
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
-import styles from './Sidebar.module.css';
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
-    return (
-        <aside className={styles.sidebar}>
-            <img className={styles.cover} 
-            src="https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" 
-            alt="" />
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+        alt=""
+      />
 
-            <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/Joaoof.png" alt="" />
+      <div className={styles.profile}>
 
-            
-                <strong>João de Deus</strong>
-                <span>Web Developer</span>
-            </div>
+      <Avatar src="https://github.com/Joaoof.png" />
 
-          <footer>
-            <a href="#">
-                <PencilLine size={20}/>
-                Editar seu perfil
-            </a>
-          </footer>
-        </aside>
-    )
+        <strong>João de Deus</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
 }
